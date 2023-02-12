021c08f326ac33de0ee18793143dfb44b615af9f
Adewumi <owonikokoadewunmi@gmail.com>
2023-02-12 22:38:41 +0100

    js_basic sentence

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 //function to multiply any two numbers
 
-const index = (a,b) => {
-    return a*b
-}
+const index = (a, b) => {
+  return a * b;
+};
 
-  console.log (index(2,3));
+console.log(index(2, 3));
+
+const firstName = "Adewumi ";
+const lastName = "Owonikoko";
+const track = "frontend";
+const skills = "Web Developer";
+const favoriteColour = "white ";
+const freedom = 1960;
+const aboutMe =
+  "My name is " +
+  firstName +
+  lastName +
+  " I am a " +
+  skills +
+  " I enrolled for stutern " +
+  track +
+  " training , my favourite colour is " +
+  favoriteColour +
+  " my country got her indepence in the year " +
+  1960;
+console.log(aboutMe);
